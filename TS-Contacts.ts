4cd8771fcec23a60a5b3dfe2fb6f9e5efca0d3de
Denis <densweetfit@gmail.com>
2023-12-23 12:56:03 +0500

    TS DZ 3

--- TS-Contacts.ts
+++ TS-Contacts.ts
@@ -1,10 +1,12 @@
-type User = {
+interface User {
+    type: 'User'
     name: string;
     age: number;
     group: string;
 };
 
-type Admin = {
+interface Admin {
+    type: 'Admin';
     name: string;
     age: number;
     role: string;
@@ -14,40 +16,57 @@ type Person = User | Admin;
 
 const persons: Person[] = [
     {
+        type: 'User',
         name: 'Иван Петров',
         age: 27,
         group: 'SEO-специалист',
     },
     {
+        type: 'User',
         name: 'Максим Лескин',
         age: 35,
         group: 'Веб - Бог',
     },
     {
+        type: 'User',
         name: 'Денис Фомин',
         age: 36,
         group: 'Затупок',
     },
     {
+        type: 'User',
         name: 'Хз Даже',
         age: 12,
         group: 'Обрубок программиста',
     },
     {
+        type: 'User',
         name: 'Марат Aляуддинов',
         age: 20,
         group: 'Музыкант',
     },
     {
+        type: 'Admin',
         name: 'Админ',
         age: 20,
         role: 'Админ',
     },
 ];
 
-const logPerson = (person: Person) => {
-    console.log(`${person.name}, ${person.age}`);
-}
+
 
 console.log('Users:');
+
+
+
+const logPerson = (person: Person) => {
+    let information: string = "";
+    if (person.type === 'Admin') {
+        information = person.role;
+    } else {
+        information = person.group;
+    }
+    console.log(`${person.name}, ${person.age}, ${information}`);
+};
+
 persons.forEach(logPerson);
--- dist/TS-Contacts.js
+++ dist/TS-Contacts.js
@@ -1,38 +1,53 @@
 "use strict";
+;
+;
 const persons = [
     {
+        type: 'User',
         name: 'Иван Петров',
         age: 27,
         group: 'SEO-специалист',
     },
     {
+        type: 'User',
         name: 'Максим Лескин',
         age: 35,
         group: 'Веб - Бог',
     },
     {
+        type: 'User',
         name: 'Денис Фомин',
         age: 36,
         group: 'Затупок',
     },
     {
+        type: 'User',
         name: 'Хз Даже',
         age: 12,
         group: 'Обрубок программиста',
     },
     {
+        type: 'User',
         name: 'Марат Aляуддинов',
         age: 20,
         group: 'Музыкант',
     },
     {
+        type: 'Admin',
         name: 'Админ',
         age: 20,
         role: 'Админ',
     },
 ];
-const logPerson = (person) => {
-    console.log(`${person.name}, ${person.age}`);
-};
 console.log('Users:');
+const logPerson = (person) => {
+    let information = "";
+    if (person.type === 'Admin') {
+        information = person.role;
+    }
+    else {
+        information = person.group;
+    }
+    console.log(`${person.name}, ${person.age}, ${information}`);
+};
 persons.forEach(logPerson);
